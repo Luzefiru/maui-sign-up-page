61e6b94c434f4c462e67f5b8b276ac34d5940c16
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-02-24 13:47:30 +0800

    fix the underline default styling for entries

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
 using Microsoft.Maui;
 using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.PlatformConfiguration;
 using Microsoft.Maui.Graphics;
 
 
@@ -21,10 +22,20 @@ namespace SignUpPage
 
 #if DEBUG
             builder.Logging.AddDebug();
+#endif  
+            Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(Entry), (handler, view) =>
+            {
+#if ANDROID
+            handler.PlatformView.SetBackgroundColor(Android.Graphics.Color.Transparent);
+#elif IOS                
+            handler.PlatformView.BackgroundColor = UIKit.UIColor.Clear; 
 #endif
+            });
+
 
             return builder.Build();
         }
+
     }
 }
 
